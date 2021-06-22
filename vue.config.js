0d30cdfb58692eb414692f4aabc7686f08545953
DESKTOP-OKBDMEM\Ledar <fatmaadl9611@gmail.com>
2021-06-22 22:02:07 +0200

    build

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    publicPath: '/To-Do-List-Vue.js/dist'
-  }
+    publicPath: "/To-Do-List-Vue.js",
+};
 // import {defineConfig } from 'vite'
 // import vue from '@vitejs/plugin-vue'
 
