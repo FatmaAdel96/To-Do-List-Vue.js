dffa9aab95ef8e03f1f6a712a3e09ad614b96624
DESKTOP-OKBDMEM\Ledar <fatmaadl9611@gmail.com>
2021-06-22 20:43:43 +0200

    no message

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+// module.exports = {
+//     publicPath: '/To-Do-List-Vue.js/dist'
+//   }
+import {defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+    base: '/To-Do-List-Vue.js',
+    plugins: [vue()]
+})
